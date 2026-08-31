281426b21d25269dbfce7606f15314df032fdc74
Himanshu <sharmah1307@gmail.com>
2026-08-31 23:16:02 +0530

    Prepare HRMS for Vercel deployment

--- db.py
+++ db.py
@@ -1,13 +1,29 @@
 import psycopg2
+import os
+
+
+
+# for deployment 
+
 
 def get_connection():
     connection = psycopg2.connect(
-        database="hrms_db",
-        user="postgres",
-        password="admin123",
-        # host="localhost",
-        host="postgres-container",
-        port="5432"
+        os.getenv("DATABASE_URL")
     )
 
     return connection
+
+
+
+
+# def get_connection():
+#     connection = psycopg2.connect(
+#         database="hrms_db",
+#         user="postgres",
+#         password="admin123",
+#         host="localhost",
+#         #host="postgres-container",
+#         port="5432"
+#     )
+
+#     return connection
